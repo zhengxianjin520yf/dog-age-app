140b496f8374ce6be7bd18e2863549f92e3440a0
zhengxianjin520yf <18171998856@163.com>
2026-06-25 08:48:29 +0800

    Add files via upload

--- script.py
+++ script.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python3
+import streamlit as st
 
-age = int(input("请输入你家狗狗的年龄: "))
-print("")
+st.title("🐶 狗狗年龄换算器")
+
+age = st.number_input("请输入你家狗狗的年龄（岁）", min_value=0, step=1, format="%d")
+
 if age <= 0:
-    print("你是在逗我吧!")
+    st.warning("你是在逗我吧! 请输入一个正数。")
 elif age == 1:
-    print("相当于 14 岁的羿芬。")
+    st.success("相当于 14 岁的羿芬。")
 elif age == 2:
-    print("相当于 22 岁的羿芬。")
-elif age > 2:
+    st.success("相当于 22 岁的羿芬。")
+else:
     human = 22 + (age - 2) * 5
-    print("对应羿芬年龄: ", human)
-
-### 退出提示
-input("点击 enter 键退出")
+    st.success(f"对应羿芬年龄: {human} 岁")
